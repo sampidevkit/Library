22fa0869a0b5a2755cf96870d9ada6805f383f5a
SAMPIDevKit <sampidevkit@gmail.com>
2023-07-12 22:52:50 +0700

    UartMultiflexer update 1

--- Common/UartMultiflexer.c
+++ Common/UartMultiflexer.c
@@ -1,4 +1,48 @@
 #include "UartMultiflexer.h"
 #include "Util.h"
 
+static bool Stuffing;
+static uint8_t PortIdx=0;
+static uint8_t TxByteCount=0;
+static uint8_t DoNext=0;
 
+void UartMux_RxTask(void)
+{
+    uint8_t c;
+
+    if(!UpStreamPort_IsRxReady())
+        break;
+
+    c=UpStreamPort_ReadByte();
+
+    switch(DoNext)
+    {
+        case 0:
+            if(c==0xFC)
+            {
+                Stuffing=0;
+                PortIdx=0;
+                DoNext++;
+            }
+            break;
+
+        case 1:
+            switch(c)
+            {
+                case 0x20:
+                    Stuffing^=1;
+                    break;
+
+                default:
+                    while(DownStreamPortCxt[PortIdx].isTxReady())
+                        
+                        break;
+            }
+            break;
+    }
+}
+
+void UartMux_TxTask(void)
+{
+
+}
--- Common/UartMultiflexer.h
+++ Common/UartMultiflexer.h
@@ -8,14 +8,13 @@ typedef struct PORT_CXT
 {
     bool (*isRxReady)(void);
     bool (*isTxReady)(void);
+    bool (*isTxDone)(void);
     uint8_t (*ReadByte)(void);
     void (*WriteByte)(uint8_t);
-    
-    struct PORT_CXT *Next;
 } port_cxt_t;
 
-#if(NUM_OF_PORTS>0)
-extern const port_cxt_t PortCxt[NUM_OF_PORTS];
+#if(NUM_OF_DOWNSTREAM_PORTS>0)
+extern const port_cxt_t DownStreamPortCxt[NUM_OF_DOWNSTREAM_PORTS];
 #endif
 
 #endif
